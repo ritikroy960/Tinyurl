1754e439bdaba989a2803399066fcecfdef1d181
ritikroy960 <132186112+ritikroy960@users.noreply.github.com>
2024-06-02 06:58:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import telebot
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import pyshorteners
 
 app = Flask(__name__)
@@ -18,8 +18,8 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(['start'])
 def send_welcome(message):
-    name = message.from_user.first_name.title()
-    bot.reply_to(message, f"Hii!👋 {name} \nWelcome to TinyUrl Bot! Send me a link and I'll Shorten it for you.", reply_markup=InlineKeyboardMarkup(start_button))
+    name = message.from_user.first_name
+    bot.reply_to(message, f"Hii👋 {name}\n Welcome to TinyUrl Shortner Bot! Send me a link and I'll Shorten it for you.", reply_markup=InlineKeyboardMarkup(start_button))
 
 @bot.message_handler(['tinyurl'])
 def send_tinyurl(message):
@@ -27,7 +27,7 @@ def send_tinyurl(message):
         url = message.text.split(' ', 1)[1]
         s = pyshorteners.Shortener()
         short_url = s.tinyurl.short(url)
-        bot.reply_to(message, 'YOUR LINK GOT SHORTENED\n ', + short_url)
+        bot.reply_to(message, 'YOUR LINK GOT SHORTENED\n ' + short_url)
 
     except:
         bot.reply_to(message, "i'm sorry, i can't capable to short bulk links")
@@ -37,8 +37,8 @@ def dend(message):
     bot.reply_to(message, "Please send Link in This Format\n /tinyurl http://example.com ")
 
 start_button = [[
-  InlineKeyboardButton("JOIN CHANNEL", url="https://t.me/devx_coder"),
-  InlineKeyboardButton("DEVELOPER", url="https://replit.com/@priyanshu999")
+  InlineKeyboardButton("JOIN UPDATE CHANNEL", url="https://t.me/devx_coder"),
+  InlineKeyboardButton("SOURCE CODE", url="https://replit.com/@priyanshu999")
 ]]
 
 bot.polling()
